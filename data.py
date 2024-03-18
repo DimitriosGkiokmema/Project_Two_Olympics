5b75bf25df6101dbd0e538d5914d0e6d11bc9486
dorcagombar <161655133+dorcagombar@users.noreply.github.com>
2024-03-18 11:28:35 -0400

    Add files via upload

---
+++ data.py
@@ -0,0 +1,38 @@
+"""
+TODO
+"""
+from __future__ import annotations
+from typing import Any
+
+
+class _Vertex:
+    """A vertex in a book review graph, used to represent a user or a book.
+
+    Each vertex item is either a user id or book title. Both are represented as strings,
+    even though we've kept the type annotation as Any to be consistent with lecture.
+
+    Instance Attributes:
+        - item: The data stored in this vertex, representing a user or book.
+        - kind: The type of this vertex: 'user' or 'book'.
+        - neighbours: The vertices that are adjacent to this vertex.
+
+    Representation Invariants:
+        - self not in self.neighbours
+        - all(self in u.neighbours for u in self.neighbours)
+        - self.kind in {'user', 'book'}
+    """
+    item: Any
+    kind: str
+    neighbours: set[_Vertex]
+
+    def __init__(self, item: Any, kind: str) -> None:
+        """Initialize a new vertex with the given item and kind.
+
+        This vertex is initialized with no neighbours.
+
+        Preconditions:
+            - kind in {'user', 'book'}
+        """
+        self.item = item
+        self.kind = kind
+        self.neighbours = set()
