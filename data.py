03ec8787759aa7960b76c3a9a5df8b9acb8a1a5a
dorcagombar <161655133+dorcagombar@users.noreply.github.com>
2024-03-18 12:18:18 -0400

    Update data.py

--- data.py
+++ data.py
@@ -2,8 +2,9 @@
 TODO
 """
 from __future__ import annotations
-from typing import Any
+
 import csv
+from typing import Any
 
 
 class _Vertex:
@@ -20,7 +21,7 @@ class _Vertex:
     Representation Invariants:
         - self not in self.neighbours
         - all(self in u.neighbours for u in self.neighbours)
-        - self.kind in {'user', 'book'}
+        - self.kind in {'year', 'country'}
     """
     item: Any
     kind: str
@@ -32,7 +33,7 @@ class _Vertex:
         This vertex is initialized with no neighbours.
 
         Preconditions:
-            - kind in {'user', 'book'}
+            - kind in {'year', 'country'}
         """
         self.item = item
         self.kind = kind
@@ -51,3 +52,89 @@ class Graph:
     def __init__(self) -> None:
         """Initialize an empty graph (no vertices or edges)."""
         self._vertices = {}
+
+    def add_vertex(self, item: Any, kind: str) -> None:
+        """Add a vertex with the given item and kind to this graph.
+
+        The new vertex is not adjacent to any other vertices.
+        Do nothing if the given item is already in this graph.
+
+        Preconditions:
+            - kind in {'year', 'country'}
+        """
+        if item not in self._vertices:
+            self._vertices[item] = _Vertex(item, kind)
+
+    def add_edge(self, item1: Any, item2: Any) -> None:
+        """Add an edge between the two vertices with the given items in this graph.
+
+        Raise a ValueError if item1 or item2 do not appear as vertices in this graph.
+
+        Preconditions:
+            - item1 != item2
+        """
+        if item1 in self._vertices and item2 in self._vertices:
+            v1 = self._vertices[item1]
+            v2 = self._vertices[item2]
+
+            v1.neighbours.add(v2)
+            v2.neighbours.add(v1)
+        else:
+            raise ValueError
+
+    def adjacent(self, item1: Any, item2: Any) -> bool:
+        """Return whether item1 and item2 are adjacent vertices in this graph.
+
+        Return False if item1 or item2 do not appear as vertices in this graph.
+        """
+        if item1 in self._vertices and item2 in self._vertices:
+            v1 = self._vertices[item1]
+            return any(v2.item == item2 for v2 in v1.neighbours)
+        else:
+            return False
+
+    def get_neighbours(self, item: Any) -> set:
+        """Return a set of the neighbours of the given item.
+
+        Note that the *items* are returned, not the _Vertex objects themselves.
+
+        Raise a ValueError if item does not appear as a vertex in this graph.
+        """
+        if item in self._vertices:
+            v = self._vertices[item]
+            return {neighbour.item for neighbour in v.neighbours}
+        else:
+            raise ValueError
+
+    def get_all_vertices(self, kind: str = '') -> set:
+        """Return a set of all vertex items in this graph.
+
+        If kind != '', only return the items of the given vertex kind.
+
+        Preconditions:
+            - kind in {'year', 'country'}
+        """
+        if kind != '':
+            return {v.item for v in self._vertices.values() if v.kind == kind}
+        else:
+            return set(self._vertices.keys())
+
+
+def load_graph(reviews_file: str, book_names_file: str) -> Graph:
+    """
+        TODO
+    """
+    graph = Graph()
+    book_dict = {}
+    with open(book_names_file, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            book_dict[row[0]] = row[1]
+
+    with open(reviews_file, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            graph.add_vertex(row[0], 'user')
+            graph.add_vertex(book_dict[row[1]], 'book')
+            graph.add_edge(row[0], book_dict[row[1]])
+    return graph
