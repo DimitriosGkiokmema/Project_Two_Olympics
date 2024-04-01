a4c9a63ac06e816c0b51306bed6b08cfef364b4e
dorcagombar <161655133+dorcagombar@users.noreply.github.com>
2024-04-01 12:07:28 -0400

    Update data.py
    sport_flow added, to compute overtime sports number

--- data.py
+++ data.py
@@ -562,15 +562,17 @@ class Graph:
         total = sum(self.medal_all_years(start_year, end_year))
         return round(total / (end_year - start_year + 1), 2)
 
-    def sport_flow(self) -> dict[int, int]:
-        """Return the total number of medals for each year from start_year to end_year, INCLUSIVE"""
+    def sport_flow(self, start_year: int, end_year: int) -> dict[int, int]:
+        """Return the total number of sports for each year from start_year to end_year, INCLUSIVE. 
+        If there is a year that is not included in self, it will be represented with sports number 0."""
         years_flow = {}
-        for y in self._vertices:
-            if self._vertices[y].kind == 'year':
-                sport_number = 0
+        for y in range(start_year, end_year + 1):
+            sport_number = 0
+            if y in self._vertices:
                 for country in self._vertices[y].neighbours:
-                    sport_number += len(self._vertices[y].neighbours[country].team_sports) + len(self._vertices[y].neighbours[country].team_sports)
-                years_flow[y] = sport_number
+                    sport_number += len(self._vertices[y].neighbours[country].team_sports) + len(
+                        self._vertices[y].neighbours[country].team_sports)
+            years_flow[y] = sport_number
         return years_flow
 
     def participation_overall_average(self) -> float | int:
